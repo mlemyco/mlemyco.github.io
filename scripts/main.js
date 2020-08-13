ed0f79a2efcbc733d5ce4130df28e63ed1296ca3
koienshi <55773406+koienshi@users.noreply.github.com>
2020-08-13 03:53:43 -0400

    parallax tweaks

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ let contactBg = document.getElementById('contact-bg');
 
 window.addEventListener('scroll', function() {
     var value = window.scrollY;
-    introBg.style.top = 200 + (value * 0.3) + 'px';
+    introBg.style.top = 100 + (value * 0.3) + 'px';
     progBg.style.top = (value * 0.3) + 'px';
     artBg.style.top = (value * 0.3) + 'px';
     contactBg.style.top = (value * 0.3) + 'px';
